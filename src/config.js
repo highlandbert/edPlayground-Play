a33b5df196a59433d07a219aed8252647243f40c
unknown <roberto.sl.90@gmail.com>
2018-04-26 01:32:26 +0200

    Loading script@

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const Config = {
-  api: 'http://localhost:8080/api'
+  api: 'http://localhost:8080/api',
+  scripts: 'http://localhost:8080/scripts'
 };
 
 export default Config
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import AuthService from './auth.service'
 import ApiService from './api.service'
+import Config from './config';
 
 if (!AuthService.hasCredentials()) {
   AuthService.doLogin();
@@ -11,11 +12,20 @@ document.getElementById('user').innerText = AuthService.getUsername();
 
 const levelId = location.search.replace('?level=', ''); // '5aa27062a8669d1a785f511b';
 
+const addScript = (id) => {
+  const script = document.createElement('script');
+  script.src = `${Config.scripts}/level_${id}`;
+  document.body.appendChild(script);
+};
+
 ApiService.get(`levels/${levelId}`).then(level => {
   console.log(level);
   document.getElementById('name').innerText = level.name;
+  addScript(levelId);
 })
 .catch(() => {
   console.log('Not Found');
   document.getElementById('e404').className = 'show';
 });
+
+
